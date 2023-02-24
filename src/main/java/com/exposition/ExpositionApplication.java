85de8c5083812f11950667e269aff70a9c0d9b4b
jongtakPark <whdxkr19@naver.com>
2023-02-24 17:28:15 +0900

    no message

---
+++ src/main/java/com/exposition/ExpositionApplication.java
@@ -0,0 +1,13 @@
+package com.exposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpositionApplication.class, args);
+	}
+
+}
