c6b893502b3721c96069ad868f34bc8fb06a0b6f
An jung-ho <ajh85000@naver.com>
2023-03-06 13:45:59 +0900

    no message

--- src/main/java/com/exposition/dto/FreeBoardDto.java
+++ src/main/java/com/exposition/dto/FreeBoardDto.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
-public class freeBoardDto {
+public class FreeBoardDto {
 
 	private Long id;
 	@NotEmpty(message = "제목을 적어주세요.")
