beed7754023fed1735ed9bdf82e325e45c54f2ad
An jung-ho <ajh85000@naver.com>
2023-03-08 17:22:54 +0900

    영규꺼랑 결합

--- src/main/java/com/exposition/repository/CompanyRepository.java
+++ src/main/java/com/exposition/repository/CompanyRepository.java
@@ -8,12 +8,7 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 	
 	Company findByCom(String string);
 	boolean existsByCom(String com);
-<<<<<<< HEAD
 	Company findByEmail(String email);
 
-=======
-	
->>>>>>> e08d36987abe61f464984a8bb3ed7d3691a5f01c
-	
 
 }
