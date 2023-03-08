eb8dce9ca41f19d7427c07b01b93e9e443e0e12f
An jung-ho <ajh85000@naver.com>
2023-03-08 17:05:44 +0900

    email 추가

--- src/main/java/com/exposition/repository/CompanyRepository.java
+++ src/main/java/com/exposition/repository/CompanyRepository.java
@@ -8,6 +8,7 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
 	
 	Company findByCom(String string);
 	boolean existsByCom(String com);
+	Company findByEmail(String email);
 
 	
 
